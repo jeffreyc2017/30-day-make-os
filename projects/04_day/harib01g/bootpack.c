a5d1bed7ddeb2cad3b8c139767f8398a4ad2cb59
Elysium Dev <elysium.dev@icloud.com>
2024-06-13 15:19:13 +0800

    04_day/harib01g

--- projects/04_day/harib01g/bootpack.c
+++ projects/04_day/harib01g/bootpack.c
@@ -1,8 +1,8 @@
-void _io_hlt(void);
-void _io_cli(void);
-void _io_out8(int port, int data);
-int _io_load_eflags(void);
-void _io_store_eflags(int eflags);
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
@@ -38,7 +38,7 @@ void HariMain(void)
 	boxfill8(p, 320, COL8_0000FF, 120,  80, 220, 180);
 
 	for (;;) {
-		_io_hlt();
+		io_hlt();
 	}
 }
 
@@ -71,16 +71,16 @@ void init_palette(void)
 void set_palette(int start, int end, unsigned char *rgb)
 {
 	int i, eflags;
-	eflags = _io_load_eflags();	/* 割り込み許可フラグの値を記録する */
-	_io_cli(); 					/* 許可フラグを0にして割り込み禁止にする */
-	_io_out8(0x03c8, start);
+	eflags = io_load_eflags();	/* 割り込み許可フラグの値を記録する */
+	io_cli(); 					/* 許可フラグを0にして割り込み禁止にする */
+	io_out8(0x03c8, start);
 	for (i = start; i <= end; i++) {
-		_io_out8(0x03c9, rgb[0] / 4);
-		_io_out8(0x03c9, rgb[1] / 4);
-		_io_out8(0x03c9, rgb[2] / 4);
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
 		rgb += 3;
 	}
-	_io_store_eflags(eflags);	/* 割り込み許可フラグを元に戻す */
+	io_store_eflags(eflags);	/* 割り込み許可フラグを元に戻す */
 	return;
 }
 
